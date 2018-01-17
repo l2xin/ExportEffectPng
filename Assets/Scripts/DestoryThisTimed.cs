b156dd7bd4960574e9ba64295ef4736828f1a9f2
l2xin <407173079@qq.com>
2018-01-17 11:23:56 +0800

    截屏
    Unity截屏插件

---
+++ Assets/Scripts/DestoryThisTimed.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestoryThisTimed : MonoBehaviour 
+{
+    public float destroyTime;
+
+	void Start () 
+    {
+        GameObject.Destroy(this.gameObject, destroyTime);
+	}
+	
+	void Update () 
+    {
+	
+	}
+}
